1e4e6c35c97165ee1813288c4e83e65a0b9a1cc1
shibaa <92931538+krystineee@users.noreply.github.com>
2025-12-19 16:01:05 +0800

    Update on myApp.js

--- myApp.js
+++ myApp.js
@@ -1,7 +1,6 @@
 const express = require('express');
+const helmet = require('helmet');
 const app = express();
-
-
 
 
 
@@ -57,5 +56,5 @@ app.get("/", function (request, response) {
 });
 let port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Your app is listening on port ${port}`);
+  console.log(`server started on port ${port}`);
 });
